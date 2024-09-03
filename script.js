ba2bde2df9f54df3207eee9f6f473633b275116e
valentinaSenno <178168883+valentinaSenno@users.noreply.github.com>
2024-09-03 12:28:14 +0200

    Create responsive grid

---
+++ script.js
@@ -0,0 +1,11 @@
+function createDivs() {
+const container = document.getElementById("container");
+
+for (let i = 0; i < 256; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+}
+
+}
+createDivs();
