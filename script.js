d2ab712331abb32cb3e039bdbdc359fbb8333fd5
valentinaSenno <178168883+valentinaSenno@users.noreply.github.com>
2024-09-03 14:37:29 +0200

    CSS to generate a responsive grid

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ function createDivs(numberOfDivs = 256) {
 const container = document.getElementById("container");
 container.innerHTML = '';
 
+const rows = Math.ceil(Math.sqrt(numberOfDivs));
+const cols = rows;
+
+container.style.setProperty('--rows', rows);
+container.style.setProperty('--cols', cols);
+
 for (let i = 0; i < numberOfDivs; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
@@ -28,7 +34,8 @@ function createGrid() {
     const resetButton = document.getElementById("reset");
     resetButton.addEventListener('click', function() {
          var userInput = prompt("Insert number of squares");
-         if (userInput > 100 || userInput < 2 || typeof userInput === "string" ) {
+         var number = Number(userInput);
+         if (isNaN(number) || userInput > 100 || userInput < 2) {
             alert("Insert number between 2 and 100");
          } else {
             numberOfDivs = userInput * userInput;
@@ -38,3 +45,4 @@ function createGrid() {
     })
 };
 createGrid();
+color();
